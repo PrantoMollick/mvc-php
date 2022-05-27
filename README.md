2d1c82b43d33bb1c4293781c6b30ad2afe85fb2e
Pranto mollick <prantocse01@gmail.com>
2022-05-28 02:45:24 +0600

    Mapping Methods & Parameters

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 # MVC Design pattern
 Direct Everything Through index_php<br>
 Direct Everything Through index_php
-Bootstrap FIle & Core Class
+Bootstrap FIle & Core Class<br>
+Mapping Methods & Parameters
--- app/controllers/Pages.php
+++ app/controllers/Pages.php
@@ -4,4 +4,12 @@
          public function __construct() {
              echo "Pages Loaded";
          }
+
+         public function index() {
+
+         }
+
+         public function about($id) {
+            echo $id;
+         }
      }
